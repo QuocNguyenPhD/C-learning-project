5efa94a6cca2a81a2e12fbca76efbedf5920e761
fbisci <fbisci123@gmail.com>
2023-01-01 12:07:21 -0600

    Create login_system.cc

---
+++ login_system.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool login(){
+	std::string username, password, ua, pa;
+
+	std::cout<< "username: "<< std::endl;
+	std::cin>> username;
+
+	std::cout<< "password: "<<std::endl;
+	std::cin>> password;
+
+	ifstream read("data\\"+username +".txt");
+	getline(read, ua);
+	getline(read, pa);
+
+	if(username== ua && password== pa){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+int main(){
+	int choice;
+	std::string username, password;
+	std::cout<< "1) register:  \n2) login:  \n*) your choice: " << std::endl;
+	std::cin>> choice ;
+
+	if(choice==1){
+		std::cout<< "username you want to register: "<< std::endl;
+		std::cin>> username;
+
+		std::cout<< "password you want to register: "<< std::endl;
+		std::cin>> password; 
+
+		ofstream file;
+		file.open("data\\"+ username +".txt");
+		file<< username<< std::endl;
+		file<< password<< std::endl;
+
+	}
+	else if(choice==2){
+		std::cout<< "username" <<std::endl;
+		std::cin>> username;
+
+		std::cout<< "password" <<std::endl;
+		std::cin>> password;
+
+		
+	}
+	else{
+		std::cout<< "option is not valid"<< std::endl;
+	}
+
+	
+
+	
+return 0;	 
+}
